f5146761f4fa0761817cfaa827029cab71204236
Oliverkor10 <okorchno@ucsd.edu>
2021-11-17 19:50:40 -0800

    linted

--- root/scripts/signIn.js
+++ root/scripts/signIn.js
@@ -1,12 +1,10 @@
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init);
 
-function init(){
-    let testUrl = "https://google.com";
-    let btn = document.getElementById('btn1')
-
-
-    btn.addEventListener('click', () => {
-        console.log("It's working");
-        window.open("../html/generalAccount.html");
-    })
+function init() {
+  const testUrl = 'https://google.com';
+  const btn = document.getElementById('btn1');
+  btn.addEventListener('click', () => {
+    console.log("It's working");
+    window.open('../html/generalAccount.html');
+  });
 }
