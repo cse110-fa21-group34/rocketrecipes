ef9e8a4073bf99703b54dab8a5cd2566916cd2c3
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-11-17 20:11:44 -0800

    updated styles for nav bar

--- root/components/custom-navbar.js
+++ root/components/custom-navbar.js
@@ -11,41 +11,63 @@ class Navbar extends HTMLElement {
     // create styles for navbar
     const style = document.createElement('style');
     style.innerHTML = `
-        .topBar {
-            display: block;
-            height: 100%;
-            margin-top: 20px;
-            text-align: right;
+        .navbar-container {
+            display: flex;
+            flex-direciton: row;
+            align-items: center;
+            justify-content: space-between;
+            
+            height: 80px;
+            font-size: 20px;
+            margin: 10px;
         }
-        .topBar button {
+        .navbar-image {
+            text-decoration: none;
+            width: 60px;
+            height 60px;
+        }
+        .navbar-image > image {
+            width: 60px;
+            height 60px;
+        }
+        .navbar-links-container {
+            display: flex;
+            align-items: center;
             margin-right: 40px;
+        }
+        .navbar-text-link {
+            margin-left: 30px;
+            color: black;
+        }
+        .navbar-button-link {
+            margin-left: 40px;
             border-radius: 12px;
             border-width: 1px;
             width: 115px;
             height: 40px;
             background-color: pink;
             font-size: 20px;
+            font-family: 'Times';
         }
-        .topBar a {
-            font-size: 20px;
-            margin-right: 40px;
-            height: 60px;
-        }
-        .topBar a > image {
-            margin-left: 20px;
+        .navbar-button-link:hover {
+            cursor: pointer;
         }
     `;
 
     // create html for navbar
     const navbarContainer = document.createElement('div');
     navbarContainer.innerHTML = `
-        <a href="./homepage.html" style="text-decoration: none; float: left;">
+        <a class="navbar-image" href="./homepage.html"> 
             <img src="../media/teamLogo.png" width="60" height="60" > 
         </a>
-        <a href="./searchpage.html">Search</a>
-        <a href="./generalAccount.html">My Account</a>
+        <div class="navbar-links-container"> 
+            <a class="navbar-text-link" href="./searchpage.html">Search</a>
+            <form action="./generalAccount.html">
+                <button class="navbar-button-link">My Account</button>
+            </form>
+        </div>
     `;
-    navbarContainer.classList.add('topBar');
+    navbarContainer.classList.add('navbar-container');
 
     this.shadowRoot.append(style, navbarContainer);
   }
