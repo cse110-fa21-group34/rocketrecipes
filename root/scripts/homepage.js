486d93f0c5179d1075d5dcdda69581615697e339
gmihir <m08.in.us@gmail.com>
2021-11-19 02:39:07 -0800

    moved functions to utils.js, added all CRUD functions

--- root/scripts/homepage.js
+++ root/scripts/homepage.js
@@ -1,5 +1,6 @@
-const COMMUNITY_RECIPE_URL = 'https://raw.githubusercontent.com/cse110-fa21-group34/rocketrecipes/main/root/scraper/recipes.json';
-const LOCAL_STORAGE_ALL_RECIPES_KEY = 'allRecipes';
+// eslint-disable-next-line import/extensions
+import { getAllRecipes } from './utils.js';
+
 let allRecipes = {};
 
 function createRecommendedRecipes() {
@@ -19,19 +20,6 @@ function createRecommendedRecipes() {
 
     recommendedRecipeContainer.appendChild(recipeCard);
   }
-}
-
-async function getAllRecipes() {
-  if (localStorage.getItem(LOCAL_STORAGE_ALL_RECIPES_KEY) !== null) {
-    const localStorageRecipes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ALL_RECIPES_KEY));
-    return localStorageRecipes;
-  }
-  const fetchedRecipes = await fetch(COMMUNITY_RECIPE_URL)
-    .then((response) => response.json())
-    .then((data) => data);
-
-  localStorage.setItem(LOCAL_STORAGE_ALL_RECIPES_KEY, JSON.stringify(fetchedRecipes));
-  return fetchedRecipes;
 }
 
 async function init() {
