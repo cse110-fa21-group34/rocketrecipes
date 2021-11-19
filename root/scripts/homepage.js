e3942d65aaad5cca0f92741e57b63c0b0f1af3ce
gmihir <m08.in.us@gmail.com>
2021-11-18 17:35:00 -0800

    added init() function, integrated with the backend

--- root/scripts/homepage.js
+++ root/scripts/homepage.js
@@ -1,338 +1,48 @@
 // this is temporary until we figure out how to fetch the data
-const recipeData = [{
-  id: 'b1ffbdfcc516588601f4ee651b5ed684',
-  title: 'Slow Cooker Chicken and Dumplings',
-  readyInMinutes: 11,
-  servings: 6,
-  image: 'https://spoonacular.com/recipeImages/715383-556x370.jpg',
-  uploader: 'From the Internet',
-  isFromInternet: true,
-  vegetarian: false,
-  vegan: false,
-  cheap: false,
-  glutenFree: false,
-  dairyFree: false,
-  quickEat: false,
-  fiveIngredientsOrLess: false,
-  easyCook: false,
-  ingredients: [{
-    name: 'butter',
-    amount: 4,
-    unit: 'tbsp',
-  }, {
-    name: 'buttermilk biscuits',
-    amount: 1,
-    unit: 'can',
-  }, {
-    name: 'condensed cream of chicken soup',
-    amount: 2,
-    unit: 'cans',
-  }, {
-    name: 'wheat flour',
-    amount: 1,
-    unit: 'Tbsp',
-  }, {
-    name: 'onion',
-    amount: 1,
-    unit: '',
-  }, {
-    name: 'parsley',
-    amount: 1,
-    unit: 'tsp',
-  }, {
-    name: 'black pepper',
-    amount: 1,
-    unit: 'tsp',
-  }, {
-    name: 'poultry seasoning',
-    amount: 2,
-    unit: 'tsp',
-  }, {
-    name: 'boneless skinless chicken breast',
-    amount: 3,
-    unit: '',
-  }, {
-    name: 'water',
-    amount: 2,
-    unit: 'cups',
-  }],
-  summary: 'Slow Cooker Chicken and Dumplings might be just the main course you are searching for. For <b>$1.45 per serving</b>, this recipe <b>covers 17%</b> of your daily requirements of vitamins and minerals. This recipe makes 6 servings with <b>519 calories</b>, <b>20g of protein</b>, and <b>28g of fat</b> each. 639 people were impressed by this recipe. From preparation to the plate, this recipe takes approximately <b>11 minutes</b>. A mixture of flour, chicken breasts, parsley, and a handful of other ingredients are all it takes to make this recipe so scrumptious. All things considered, we decided this recipe <b>deserves a spoonacular score of 73%</b>. This score is solid. Try <a href="https://spoonacular.com/recipes/slow-cooker-chicken-and-dumplings-94710">Slow Cooker Chicken and Dumplings</a>, <a href="https://spoonacular.com/recipes/slow-cooker-chicken-and-dumplings-596946">Slow Cooker Chicken and Dumplings</a>, and <a href="https://spoonacular.com/recipes/slow-cooker-chicken-and-dumplings-686869">Slow Cooker Chicken and Dumplings</a> for similar recipes.',
-  steps: [{
-    number: 1,
-    step: 'Place Slow Cooker on high.',
-  }, {
-    number: 2,
-    step: 'Add chicken breasts, butter, water, soup, flour, onion, poultry seasoning, pepper, and parsley. Stir and mix well. Cook on high for 4 hours. Take two forks and start shredding chicken in the mixture.After shredding chicken, tear small pieces of biscuit dough and place in slow cooker for remaining 2 hours. Stirring occasionally.',
-  }],
-}, {
-  id: 'bd23bca6a5f969c0718bef3d778b1a48',
-  title: 'Beef Carrot Stew',
-  readyInMinutes: 45,
-  servings: 1,
-  image: 'https://spoonacular.com/recipeImages/769754-556x370.jpg',
-  uploader: 'From the Internet',
-  isFromInternet: true,
-  vegetarian: false,
-  vegan: false,
-  cheap: false,
-  glutenFree: true,
-  dairyFree: true,
-  quickEat: true,
-  fiveIngredientsOrLess: false,
-  easyCook: false,
-  ingredients: [{
-    name: 'bay leaves',
-    amount: 1,
-    unit: 'tablespoon',
-  }, {
-    name: 'beef',
-    amount: 8,
-    unit: 'Pieces',
-  }, {
-    name: 'carrot',
-    amount: 7,
-    unit: 'medium pieces',
-  }, {
-    name: 'red curry paste',
-    amount: 0.5,
-    unit: 'teaspoon',
-  }, {
-    name: 'garlic',
-    amount: 1,
-    unit: 'clove',
-  }, {
-    name: 'ginger',
-    amount: 1,
-    unit: 'teaspoon',
-  }, {
-    name: 'ice',
-    amount: 1,
-    unit: 'cubes',
-  }, {
-    name: 'onion',
-    amount: 1,
-    unit: 'small',
-  }, {
-    name: 'pepper and onion mix',
-    amount: 2,
-    unit: '',
-  }, {
-    name: null,
-    amount: 2,
-    unit: '',
-  }],
-  summary: "Beef Carrot Stew might be just the soup you are searching for. For <b>83 cents per serving</b>, this recipe <b>covers 7%</b> of your daily requirements of vitamins and minerals. Watching your figure? This gluten free, dairy free, and whole 30 recipe has <b>101 calories</b>, <b>3g of protein</b>, and <b>4g of fat</b> per serving. If you have seasoning cubes, curry, bulb of onion, and a few other ingredients on hand, you can make it. 171 person were impressed by this recipe. It will be a hit at your <b>Autumn</b> event. From preparation to the plate, this recipe takes around <b>45 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 45%</b>. This score is pretty good. Try <a href=\"https://spoonacular.com/recipes/oven-braised-beef-stew-with-carrot-parsnip-and-lancinato-kale-43670\">Oven-braised Beef Stew With Carrot, Parsnip, And Lancinato Kale</a>, <a href=\"https://spoonacular.com/recipes/a-winters-walk-beef-and-carrot-stew-with-herb-crusted-dumplings-101808\">A Winter's Walk Beef and Carrot Stew With Herb Crusted Dumplings</a>, and <a href=\"https://spoonacular.com/recipes/hearty-beef-stew-a-filling-beef-stew-can-be-made-easily-in-your-crockpot-599542\">Hearty Beef Stew – a filling beef stew can be made easily in your crockpot</a> for similar recipes.",
-  steps: [{
-    number: 1,
-    step: 'Scrape, wash and boil your carrots till soft.',
-  }, {
-    number: 2,
-    step: 'Place in a blender without water and grind till completely shredded.In a pot, heat up your oil and fry your onions, garlic and ginger.',
-  }, {
-    number: 3,
-    step: 'Pour in your Tatashe-pepper mix and your beef and allow to simmer on low heat for about 10-15 minutes.Season with curry, seasoning cubes and anything else you may wish to add.',
-  }, {
-    number: 4,
-    step: 'Pour in your carrots and stir in and allow to simmer on low heat for another 3-5 minutes.',
-  }, {
-    number: 5,
-    step: 'Add your chopped scent leaves and stir in and serve hot with rice, yam, plantain, pasta or any other accompaniment of your choice. ',
-  }],
-}, {
-  id: 'ae94d68f70216f7017e6056291dc2682',
-  title: 'Arugula Walnut Pesto',
-  readyInMinutes: 45,
-  servings: 4,
-  image: 'https://spoonacular.com/recipeImages/632790-556x370.jpg',
-  uploader: 'From the Internet',
-  isFromInternet: true,
-  vegetarian: false,
-  vegan: false,
-  cheap: false,
-  glutenFree: true,
-  dairyFree: false,
-  quickEat: true,
-  fiveIngredientsOrLess: false,
-  easyCook: false,
-  ingredients: [{
-    name: 'arugula',
-    amount: 2,
-    unit: 'handfuls',
-  }, {
-    name: 'garlic',
-    amount: 1,
-    unit: 'cloves',
-  }, {
-    name: 'walnuts',
-    amount: 1,
-    unit: 'small handful',
-  }, {
-    name: 'extra virgin olive oil',
-    amount: 1,
-    unit: 'serving',
-  }, {
-    name: 'lemon juice',
-    amount: 1,
-    unit: '',
-  }, {
-    name: 'parmesan',
-    amount: 1,
-    unit: 'small handful',
-  }, {
-    name: 'salt and pepper',
-    amount: 1,
-    unit: 'serving',
-  }],
-  summary: "You can never have too many condiment recipes, so give Arugula Walnut Pesto a try. This recipe serves 4. One portion of this dish contains about <b>2g of protein</b>, <b>9g of fat</b>, and a total of <b>86 calories</b>. For <b>44 cents per serving</b>, this recipe <b>covers 3%</b> of your daily requirements of vitamins and minerals. 28 people have made this recipe and would make it again. A mixture of arugula, garlic, walnuts, and a handful of other ingredients are all it takes to make this recipe so yummy. It is a good option if you're following a <b>gluten free and primal</b> diet. From preparation to the plate, this recipe takes approximately <b>approximately 45 minutes</b>. It is brought to you by Foodista. Taking all factors into account, this recipe <b>earns a spoonacular score of 58%</b>, which is pretty good. Similar recipes are <a href=\"https://spoonacular.com/recipes/arugula-walnut-pesto-489854\">Arugula Walnut Pesto</a>, <a href=\"https://spoonacular.com/recipes/arugula-walnut-pesto-8289\">Arugula Walnut Pesto</a>, and <a href=\"https://spoonacular.com/recipes/arugula-and-walnut-pesto-476027\">Arugulan and Walnut Pesto</a>.",
-  steps: [{
-    number: 1,
-    step: 'With this recipe you play with the amount of ingredients based on your likes & the strength of the garlic, arugula, etc.',
-  }, {
-    number: 2,
-    step: 'Best to use a mortar & pestle but if you dont have one the food processor works just fine.',
-  }, {
-    number: 3,
-    step: 'In the mortal & pestle or the processor start off with your arugula and a drizzle of oil to get it working. Once pulverized add in 1 clove of garlic & continue to grind. Now add in your nuts, lemon juice and a bit of salt & pepper & give it one more grind. At this point abandon the mortal & pestle or the machine & dump the mixture into a bowl.',
-  }, {
-    number: 4,
-    step: 'Add a small handful of cheese and while continuously mixing add in the olive oil until you achieve your desired consistency. For crostini I would leave as a paste, for a pasta sauce keep it a bit looser - this is all based on the amount of olive oil you use.',
-  }, {
-    number: 5,
-    step: 'Now recheck your seasonings - more garlic? A little light on the acid?',
-  }, {
-    number: 6,
-    step: 'Add a squeeze of lemon. Is it flat? A pinch of salt will wake it up.',
-  }, {
-    number: 7,
-    step: 'Let sit for an hour or so to let the flavors incorporate. Store in the fridge. It will last a few days before the arugula will turn.',
-  }, {
-    number: 8,
-    step: 'Serve tossed over fresh pasta or rubbed over toasty bread with a ribbon of parmesan, a delicious topping for pizza or a beautiful frittata with a dollop of arugula pesto! There are a million ways to use this - enjoy!',
-  }],
-}, {
-  id: '79cc3d016cb9c49997ef19b0d2578d35',
-  title: 'Coffee-braised Short Ribs',
-  readyInMinutes: 45,
-  servings: 5,
-  image: 'https://spoonacular.com/recipeImages/639891-556x370.jpg',
-  uploader: 'From the Internet',
-  isFromInternet: true,
-  vegetarian: false,
-  vegan: false,
-  cheap: false,
-  glutenFree: true,
-  dairyFree: true,
-  quickEat: true,
-  fiveIngredientsOrLess: false,
-  easyCook: false,
-  ingredients: [{
-    name: 'ancho chili powder',
-    amount: 1.5,
-    unit: 'tablespoons',
-  }, {
-    name: 'canned tomatoes',
-    amount: 14,
-    unit: '',
-  }, {
-    name: 'dark brown sugar',
-    amount: 2,
-    unit: 'tablespoons',
-  }, {
-    name: 'cilantro',
-    amount: 5,
-    unit: 'servings',
-  }, {
-    name: 'garlic',
-    amount: 6,
-    unit: '',
-  }, {
-    name: 'cumin',
-    amount: 1.25,
-    unit: 'teaspoons',
-  }, {
-    name: 'jalapeno pepper',
-    amount: 1,
-    unit: 'large',
-  }, {
-    name: 'olive oil',
-    amount: 2,
-    unit: 'tablespoons',
-  }, {
-    name: 'oregano',
-    amount: 2,
-    unit: 'teaspoons',
-  }, {
-    name: 'red pepper',
-    amount: 1,
-    unit: 'large',
-  }, {
-    name: 'bone in beef short ribs',
-    amount: 5,
-    unit: 'pounds',
-  }, {
-    name: 'coffee',
-    amount: 2,
-    unit: 'cups',
-  }, {
-    name: 'tomato paste',
-    amount: 1,
-    unit: 'tablespoon',
-  }, {
-    name: 'yellow onion',
-    amount: 1,
-    unit: 'large',
-  }],
-  summary: "Coffee-braised Short Ribs might be just the main course you are searching for. For <b>$5.55 per serving</b>, this recipe <b>covers 32%</b> of your daily requirements of vitamins and minerals. This recipe makes 5 servings with <b>1390 calories</b>, <b>49g of protein</b>, and <b>126g of fat</b> each. 6 people have tried and liked this recipe. If you have tomato paste, cilantro, brown sugar, and a few other ingredients on hand, you can make it. To use up the strong coffee you could follow this main course with the <a href=\"https://spoonacular.com/recipes/coffee-coffee-cake-537443\">Coffee Coffee Cake</a> as a dessert. It is a good option if you're following a <b>gluten free and dairy free</b> diet. All things considered, we decided this recipe <b>deserves a spoonacular score of 88%</b>. This score is amazing. Try <a href=\"https://spoonacular.com/recipes/coffee-braised-short-ribs-386092\">Coffee-Braised Short Ribs</a>, <a href=\"https://spoonacular.com/recipes/coffee-braised-short-ribs-492600\">Coffee-Braised Short Ribs</a>, and <a href=\"https://spoonacular.com/recipes/michaels-coffee-braised-short-ribs-329926\">Michael's Coffee Braised Short Ribs</a> for similar recipes.",
-  steps: [{
-    number: 1,
-    step: 'Food',
-  }, {
-    number: 2,
-    step: 'Preheat oven to 300F.',
-  }, {
-    number: 3,
-    step: 'Heat oil in a large heavy stockpot over medium-high heat.',
-  }, {
-    number: 4,
-    step: 'Sprinkle short ribs with salt and pepper. Working in batches, add ribs to pot and cook until browned, about 4 minutes per side.',
-  }, {
-    number: 5,
-    step: 'Transfer to platter.',
-  }, {
-    number: 6,
-    step: 'Add onion, red bell pepper, and jalapeo to drippings in stockpot. Reduce heat to medium, cover, and cook until onion is tender, stirring occasionally, about 6 minutes.Stir in garlic and saut uncovered 1 minute.',
-  }, {
-    number: 7,
-    step: 'Add brown sugar, ancho chile powder, oregano, and cumin; stir 15 seconds. Stir in coffee, tomatoes with juice, and tomato paste. Bring to boil, scraping up browned bits. Return ribs and any juices to pot; bring to boil.Cover and bake until meat is very tender, about 1 hour 45 minutes. Spoon fat from surface of sauce. Season sauce to taste with salt and pepper.',
-  }, {
-    number: 8,
-    step: 'Transfer ribs to a platter and spoon sauce over top.',
-  }, {
-    number: 9,
-    step: 'Sprinkle with chopped cilantro.',
-  }],
-}];
+const COMMUNITY_RECIPE_URL = 'https://raw.githubusercontent.com/cse110-fa21-group34/rocketrecipes/main/root/scraper/recipes.json';
+const LOCAL_STORAGE_RECIPE_KEY = 'localRecipes';
+let communityRecipes = {};
+let localRecipes = {};
+let allRecipes = {};
 
 function createRecommendedRecipes() {
   // fetch data for recommended recipes
   // manually fetching for now until the backend functions are done
-  const recommendedRecipeList = recipeData;
   // fetch div for recommended recipes
   const recommendedRecipeContainer = document.getElementById('recommendedRecipeContainer');
   recommendedRecipeContainer.style.display = 'flex';
   recommendedRecipeContainer.style.maxWidth = '100%';
   recommendedRecipeContainer.style.flexWrap = 'wrap';
 
-  // create new recipe card for each object from recommendedRecipeList
-  recommendedRecipeList.forEach((recipe) => {
+  const randomNumber = Math.floor(Math.random() * (allRecipes.length - 5));
+
+  for (let i = 0; i < 4; i += 1) {
     const recipeCard = document.createElement('recipe-card');
-    recipeCard.data = recipe;
+    recipeCard.data = allRecipes[randomNumber + i];
 
     recommendedRecipeContainer.appendChild(recipeCard);
-  });
+  }
 }
 
-function init() {
+async function getCommunityRecipes() {
+  return fetch(COMMUNITY_RECIPE_URL)
+    .then((response) => response.json())
+    .then((data) => data);
+}
+
+function getLocalRecipes() {
+  return localStorage.getItem(LOCAL_STORAGE_RECIPE_KEY);
+}
+
+async function init() {
+  communityRecipes = await getCommunityRecipes();
+  localRecipes = getLocalRecipes();
+
+  if (localRecipes) {
+    allRecipes = communityRecipes.concat(JSON.parse(localRecipes));
+  } else {
+    allRecipes = communityRecipes;
+  }
   createRecommendedRecipes();
 }
 
