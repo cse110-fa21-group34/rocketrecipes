4dd7d01339efb72fe4dcdc63a5b8c140eab6703e
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-11-21 13:49:53 -0800

    added edge case fields

--- root/scripts/searchpage.js
+++ root/scripts/searchpage.js
@@ -4,15 +4,16 @@ import { search } from './utils.js';
 // takes the current recipe object and fills the html of the page with
 // the information within it
 function fillSearchPage(searchResults) {
+  const searchResultsContainer = document.getElementById('search-results-container');
+  searchResultsContainer.style.display = 'flex';
+  searchResultsContainer.style.maxWidth = '100%';
+  searchResultsContainer.style.justifyContent = 'center';
+  searchResultsContainer.style.flexWrap = 'wrap';
   if (searchResults.length === 0) {
-    // no results
+    searchResultsContainer.innerHTML = `
+      <p>Sorry, no results were found for your search</p>
+    `;
   } else {
-    const searchResultsContainer = document.getElementById('search-results-container');
-    searchResultsContainer.style.display = 'flex';
-    searchResultsContainer.style.maxWidth = '100%';
-    searchResultsContainer.style.justifyContent = 'center';
-    searchResultsContainer.style.flexWrap = 'wrap';
-
     searchResults.forEach((recipe) => {
       const recipeCard = document.createElement('recipe-card');
       recipeCard.data = recipe;
@@ -27,8 +28,16 @@ async function init() {
 
   const searchParams = new URLSearchParams(queryString);
   const searchQuery = searchParams.get('searchQuery');
+  if (searchQuery === null || searchQuery === undefined) {
+    const searchResultsContainer = document.getElementById('search-results-container');
+    searchResultsContainer.style.display = 'flex';
+    searchResultsContainer.style.maxWidth = '100%';
+    searchResultsContainer.style.justifyContent = 'center';
+    searchResultsContainer.style.flexWrap = 'wrap';
 
-  if (searchQuery === null) {
+    searchResultsContainer.innerHTML = `
+      <p>Enter your search term above!</p>
+    `;
     // handle bad request
     // show empty page with note that we can't find that id
   } else {
