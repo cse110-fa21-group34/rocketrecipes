e5fc20da6d9f56529100f3a37fb984ce028d8fbd
gmihir <m08.in.us@gmail.com>
2021-11-21 00:35:41 -0800

    testing with dummy file and recursion enabled

---
+++ root/scripts/jsdocstest.js
@@ -0,0 +1,9 @@
+/**
+ * A sum function to test JSDocs
+ * @param {Number} a - the first number to add
+ * @param {Number} b - the second number to add
+ * @returns {Number} The sum of the two parameters
+ */
+function sum(a, b) {
+  return a + b;
+}
