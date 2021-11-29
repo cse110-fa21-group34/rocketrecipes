6263a01282b6593da1fc9c1665a636c4585e7979
inhui8 <yis027@ucsd.edu>
2021-11-28 18:51:38 -0800

    share-button

--- root/scripts/recipepage.js
+++ root/scripts/recipepage.js
@@ -142,6 +142,14 @@ async function init() {
   } else {
     button.style = 'color:grey';
   }
+
+  // (function(d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0];
+  //   if (d.getElementById(id)) return;
+  //   js = d.createElement(s); js.id = id;
+  //   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+  //   fjs.parentNode.insertBefore(js, fjs);
+  //   }(document, 'script', 'facebook-jssdk'));
 }
 
 window.addEventListener('DOMContentLoaded', init);
