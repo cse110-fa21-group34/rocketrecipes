9a8dd5b942c824a8fc1d527676654344ffac017d
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-11-28 22:59:07 -0800

    added first crud test with mocked fetch and local storage

---
+++ testing/unit/CRUD.test.js
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import * as utils from './testutil.js'
+import { getAllRecipes } from '../../root/scripts/utils.js';
+// const recipes = require('../../root/scraper/recipes.json');
+
+// unmockedFetch stores the original global fetch function
+const unmockedFetch = global.fetch
+
+beforeAll(() => {
+    // set local storage to be the mocked local storage
+    global.localStorage = new utils.LocalStorageMock();
+
+    // set fetch to be this mocked fetch that always returns utils.mockedRecipes
+    global.fetch = () =>
+    Promise.resolve({
+      json: () => Promise.resolve(utils.mockedRecipes),
+    })
+});
+
+test('fetches recipes', async () => {
+    const recipes = await getAllRecipes();
+    expect(recipes.length).toBeGreaterThan(0);
+});
+
+
+// After all tests are done, restore the global fetch function
+// back to the original 
+afterAll(() => {
+  global.fetch = unmockedFetch
+})
