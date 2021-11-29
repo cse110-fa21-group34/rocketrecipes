9ddc15203591ddd1fa2e4f92329e8ff102ba5cd5
gmihir <m08.in.us@gmail.com>
2021-11-29 13:41:03 -0800

    trying different github action

--- testing/e2e/home.test.js
+++ testing/e2e/home.test.js
@@ -1,7 +1,7 @@
 const rootUrl = '127.0.0.1:5501';
 
 beforeAll(async () => {
-    const result = /refs\/pull\/(\d+)\/merge/g.exec(process.env.GITHUB_REF);
+    const result = /refs\/pull\/(\d+)\/merge/g.exec(process.env.GITHUB_PR_NUMBER);
     try {
       if (!result) throw new Error("Reference not found.");
       const [, pullRequestId] = result;
