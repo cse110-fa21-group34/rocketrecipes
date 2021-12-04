090acb6ef53c53b33b963c43a62a48794fd07468
PatrickBrown1 <patrickbrown1312@gmail.com>
2021-12-04 13:07:35 -0800

    fixed page x-overflow

--- root/components/custom-navbar.js
+++ root/components/custom-navbar.js
@@ -16,10 +16,9 @@ class Navbar extends HTMLElement {
             display: flex;
             flex-direciton: row;
             align-items: center;
+            width: 100%;
             justify-content: space-between;
-            
             height: 80px;
-            width: 100vw;
             font-size: 20px;
             box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.15);
             z-index: 1000;
@@ -48,14 +47,14 @@ class Navbar extends HTMLElement {
         }
         .mobile-navbar-expanded {
             position: absolute;
-            
+            width: 100%;
             top: 80px;
             display: flex;
             flex-direction: column;
             box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.15);
         }
         .mobile-link {
-            width: 100vw;
+            width: 100%;
             padding: 0;
         }
         .mobile-navbar-button {
